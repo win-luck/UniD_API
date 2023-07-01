c35a079846a8048a724c795aa4b0351ed56ae12c
CHAE <chaso1495@naver.com>
2023-07-01 21:12:52 +0900

    [init] first commit

---
+++ src/test/java/com/springmvc/unid/UnidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.unid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
