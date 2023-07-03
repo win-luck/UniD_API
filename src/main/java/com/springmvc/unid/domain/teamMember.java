5c9f9184b37f0329a8bb8f74ac6a00bfc26830aa
CHAE <chaso1495@naver.com>
2023-07-03 17:31:56 +0900

    [fix] #7 teamMember JPA Entity로 변환

--- src/main/java/com/springmvc/unid/domain/teamMember.java
+++ src/main/java/com/springmvc/unid/domain/teamMember.java
@@ -3,18 +3,25 @@ package com.springmvc.unid.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
 @Setter
 @Getter
 public class teamMember { // Team과 User의 다대다 관계로 인해 생성된 테이블
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성
     private Long id; // 자체 id - 기본키
 
+    @Column(name = "team_name", nullable = false)
     private String teamName; // 팀명 - 외래키
 
+    @Column(name = "user_id", nullable = false)
     private String userId; // 사용자 id - 외래키
 
+    @Column(name = "join_date", nullable = false)
     private LocalDate joinDate; // 팀 가입일
 
 }
