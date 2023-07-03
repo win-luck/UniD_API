c98f0d182a310c4ca1765fe870e0fdd1cedb1965
CHAE <chaso1495@naver.com>
2023-07-03 20:40:44 +0900

    [fix] #7 Team, User, teamMember JPA Entity로 변환

--- src/main/java/com/springmvc/unid/domain/Team.java
+++ src/main/java/com/springmvc/unid/domain/Team.java
@@ -1,27 +1,35 @@
 package com.springmvc.unid.domain;
 
-import com.springmvc.unid.common.Pair;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashMap;
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
 @Getter
 @Setter
+@Table(name = "team")
 public class Team {
 
+    @Id
     private String name; // 팀명 - 기본키
 
+    @Column(nullable = false)
     private String oneLine; // 팀의 한줄설명
 
+    @Column(nullable = false)
     private String description; // 팀의 구체적 설명
 
+    @Column(nullable = false)
     private String university; // 팀의 소속 대학
 
+    @Column(nullable = true)
     private String link; // 팀의 링크
 
-    private List<requirement> require; // 팀원 모집 요구사항 (1:N)
+    @OneToMany(mappedBy = "team")
+    private List<Requirement> requirementList; // 팀원 모집 요구사항 (1:N)
 
-    private List<teamMember> teamMembers; // 소속된 팀원 명단 (N:N)
+    @OneToMany(mappedBy = "team")
+    private List<teamMember> teamMembersList; // 소속된 팀원 명단 (N:N)
 }
--- src/main/java/com/springmvc/unid/domain/User.java
+++ src/main/java/com/springmvc/unid/domain/User.java
@@ -7,26 +7,35 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "user")
 @Getter
 @Setter
 @NoArgsConstructor
 public class User {
 
     @Id
-    private String id; // 사용자 id
+    private String user_id; // user id - 기본키
 
+    @Column(nullable = false)
     private String name; // 사용자 별명
 
+    @Column(nullable = false)
     private String pw; // 사용자 비밀번호
 
+    @Column(nullable = false)
     private String university; // 사용자 소속 대학
 
+    @Column(nullable = false)
     private String major; // 사용자의 소속 학과
 
+    @Column(nullable = false)
     private String link; // 사용자의 링크
 
-    private List<userNotify> myNotifies; // 사용자가 가지고 있는 알림 명단 (N:N)
+    @OneToMany(mappedBy = "user")
+    private List<userNotify> userNotify; // 사용자가 가지고 있는 알림 명단 (N:N)
 
-    private List<teamMember> teamList; // 사용자가 소속된 팀 명단 (N:N)
+    @OneToMany(mappedBy = "user")
+    private List<teamMember> teamMember; // 사용자가 소속된 팀 명단 (N:N)
 }
 
