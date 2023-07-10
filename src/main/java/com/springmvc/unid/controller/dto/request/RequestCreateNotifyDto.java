8b0f1c1f46fba257ff920498ac5764c7734cba78
CHAE <chaso1495@naver.com>
2023-07-11 00:15:32 +0900

    [feat] #23 Notify 요청-응답 dto 구현

---
+++ src/main/java/com/springmvc/unid/controller/dto/request/RequestCreateNotifyDto.java
@@ -0,0 +1,26 @@
+package com.springmvc.unid.controller.dto.request;
+
+import com.springmvc.unid.domain.Notify;
+import lombok.Data;
+
+/**
+ * RequestCreateNotifyDto의 용도
+ * 알림 생성 요청 객체 (request)
+ */
+@Data
+public class RequestCreateNotifyDto {
+
+    private Long type; // 알림의 종류
+    private String sender; // 발신자의 id
+    private String teamName; // 발신자가 지원한(소속된) 팀
+    private String contents; // 알림(지원서, 가입승인, 가입거절, 탈퇴, 종료) 내용
+    private String link; // 지원자(승인한 팀장)이 첨부한 링크
+
+    public RequestCreateNotifyDto(Notify notify) {
+        this.type = notify.getType();
+        this.sender = notify.getUser().getName();
+        this.teamName = notify.getTeam().getName();
+        this.contents = notify.getContents();
+        this.link = notify.getLink();
+    }
+}
