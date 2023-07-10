17d6a62cbf04a14bd44b4e6b82a9afd1d6627b9c
CHAE <chaso1495@naver.com>
2023-07-10 20:53:24 +0900

    [feat] #23 ApiResponse 객체 구현

--- src/main/java/com/springmvc/unid/util/api/ApiResponse.java
+++ src/main/java/com/springmvc/unid/util/api/ApiResponse.java
@@ -1,6 +1,33 @@
 package com.springmvc.unid.util.api;
 
 
-public class ApiResponse {
+import com.springmvc.unid.util.exception.ResponseCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+
+    private ApiHeader header;
+    private ApiBody body;
+
+    private static final int SUCCESS = 200;
+
+    public ApiResponse(ApiHeader header, ApiBody body) {
+        this.header = header;
+        this.body = body;
+    }
+
+    public ApiResponse(ApiHeader header){
+        this.header = header;
+    }
+
+    public static <T> ApiResponse<T> success(T data){
+        return new ApiResponse<T>(new ApiHeader(SUCCESS, "SUCCESS"), new ApiBody(data, null));
+    }
+
+    public static <T> ApiResponse<T> fail(ResponseCode responseCode){
+        return new ApiResponse(new ApiHeader(responseCode.getHttpStatusCode(), responseCode.getMessage()), new ApiBody(null, responseCode.getMessage()));
+    }
 }
