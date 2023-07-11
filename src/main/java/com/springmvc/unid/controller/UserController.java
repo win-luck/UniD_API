03c41a260b1972f582c8e9c3b8638afec720e075
CHAE <chaso1495@naver.com>
2023-07-12 00:53:28 +0900

    [fix] #29 UserController가 팀 가입/탈퇴/조회하도록 수정

--- src/main/java/com/springmvc/unid/controller/UserController.java
+++ src/main/java/com/springmvc/unid/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.springmvc.unid.controller;
 
+import com.springmvc.unid.controller.dto.TeamDto;
 import com.springmvc.unid.controller.dto.UserDto;
+import com.springmvc.unid.controller.dto.request.RequestExitTeamDto;
+import com.springmvc.unid.controller.dto.request.RequestJoinTeamDto;
 import com.springmvc.unid.controller.dto.request.RequestLoginDto;
-import com.springmvc.unid.controller.dto.request.RequestNewUserDto;
+import com.springmvc.unid.controller.dto.request.RequestCreateUserDto;
+import com.springmvc.unid.service.TeamService;
 import com.springmvc.unid.service.UserService;
 import com.springmvc.unid.util.api.ApiResponse;
 import com.springmvc.unid.util.exception.ResponseCode;
@@ -15,7 +19,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
-    public final UserService userService;
+    private final UserService userService;
+    private final TeamService teamService;
 
     // 로그인 V
     @PostMapping("/api/users/login")
@@ -27,7 +32,7 @@ public class UserController {
 
     // 회원가입(+중복 user 방지) V
     @PostMapping("/api/users")
-    public ApiResponse<Long> join(@RequestBody RequestNewUserDto userDto){
+    public ApiResponse<Long> join(@RequestBody RequestCreateUserDto userDto){
         Long id = userService.join(userDto);
         if(id == null) return ApiResponse.fail(ResponseCode.DUPLICATED_USER);
         return ApiResponse.success(id, ResponseCode.SIGNUP_SUCCESS.getMessage());
@@ -45,6 +50,27 @@ public class UserController {
     public ApiResponse<Long> update(@PathVariable("id") Long id, @RequestBody UserDto userDto){
         userService.update(id, userDto.getName(), userDto.getUniversity(), userDto.getMajor(), userDto.getLink());
         return ApiResponse.success(1L, ResponseCode.USER_UPDATE_SUCCESS.getMessage());
+    }
+
+    // user가 특정 팀에 가입
+    @PostMapping("/api/users/{id}/teams")
+    public ApiResponse<Long> joinTeam(@PathVariable("id") Long id, @RequestBody RequestJoinTeamDto requestJoinTeamDto){
+        teamService.joinTeam(id, requestJoinTeamDto.getTeamId());
+        return ApiResponse.success(requestJoinTeamDto.getTeamId(), ResponseCode.TEAM_JOIN_SUCCESS.getMessage());
+    }
+
+    // user가 현재 소속된 팀 조회
+    @GetMapping("/api/users/{id}/teams")
+    public ApiResponse<List<TeamDto>> getTeamList(@PathVariable("id") Long id){
+        List<TeamDto> teamDtoList = teamService.findTeamByUser(userService.findOne(id));
+        return ApiResponse.success(teamDtoList, ResponseCode.TEAM_READ_SUCCESS.getMessage());
+    }
+
+    // user가 현재 소속된 팀에서 탈퇴 (팀장이 아닌 팀원만 가능)
+    @PostMapping("/api/users/{id}/teams/leave")
+    public ApiResponse<Long> leaveTeam(@PathVariable("id") Long id, @RequestBody RequestExitTeamDto requestExitTeamDto){
+        teamService.leaveTeam(id, requestExitTeamDto.getTeamId());
+        return ApiResponse.success(requestExitTeamDto.getTeamId(), ResponseCode.TEAM_LEAVE_SUCCESS.getMessage());
     }
 
     // 회원 정보 조회 V
