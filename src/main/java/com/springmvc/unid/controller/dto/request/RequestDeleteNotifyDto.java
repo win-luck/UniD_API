3f348d9e714ec0534172e58bb123ffec0ff6c47d
CHAE <chaso1495@naver.com>
2023-07-13 21:32:24 +0900

    [feat] #29 Notify 생성/삭제 요청 Dto 추가

---
+++ src/main/java/com/springmvc/unid/controller/dto/request/RequestDeleteNotifyDto.java
@@ -0,0 +1,10 @@
+package com.springmvc.unid.controller.dto.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class RequestDeleteNotifyDto {
+    private Long notifyId; // 알림의 id
+}
